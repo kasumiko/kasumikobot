6c7077a8f9a8831928a6018c125e7fb58e41bfb1
kasumiko <ponywoodcommon@gmail.com>
2014-08-29 16:41:55 +0900

    ranker
    修正

--- ranker.rb
+++ ranker.rb
@@ -2,23 +2,21 @@
 # encoding: utf-8
 require'pstore'
 require'pp'
-db = PStore.new('counter.ps')
+db = PStore.new('~/document/kasumikobot/counter.ps')
 db.transaction do
  data = Hash::new
  db.roots.each{|i|
-  data[i] = db[i].to_i
+  data[i] = db[i]
  }
-
  datus = []
  data.sort_by{|key,val| val}.each{|m|
   m << data.sort_by{|key,val| val}.index(m)+1
   datus << m
  }
- rank = PStore.new('ranker.ps')
+ rank = PStore.new('~/document/kasumikobot/ranker.ps')
  rank.transaction do
   datus.each{|n|
    rank[n[0]]=n[2]
   }
-pp rank
 end
 end
