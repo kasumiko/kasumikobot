7416d1d88d80bc46b7a1a7e10ebb3bc1cddec346
kasumiko <ponywoodcommon@gmail.com>
2014-09-06 22:13:17 +0900

    いらないファイルを消した
    ぽいぽい

--- test.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-contents = "test"
-  puts contents
-open(File.expand_path('../test1.txt',__FILE__),'w') do |f|
-   f.write contents
-  end 
--- test1.rb
+++
@@ -1,4 +0,0 @@
-# encoding: utf-8
-require 'pp'
-pp Dir.entries("./yui")
-
