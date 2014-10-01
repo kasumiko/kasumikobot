17acfc69972f909b410b172727ed843ce710aff9
kasumiko <ponywoodcommon@gmail.com>
2014-10-02 02:50:40 +0900

    attend
    (◞‸◟)

--- attend.rb
+++ attend.rb
@@ -2,12 +2,12 @@
 require'pstore'
 require'pp'
 #出席管理
-def school_attend(contents,username,id,status)
+def school_attend(contents,username,id)
 contents = contents.gsub(/@kasumikobot|\s|　/,"").tr('１-９','1-9')
  attend = PStore.new('attend.ps')
  attend.transaction do
   if attend[username] == nil
-   attend[username]=[[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],]
+   attend[username]=[[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],]
   end
   i,j,k = nil
   case contents[0,2]
@@ -41,18 +41,22 @@ contents = contents.gsub(/@kasumikobot|\s|　/,"").tr('１-９','1-9')
   k = 0
   when "欠席"
   k = 1
-  when "休講"
+  when "遅刻"
   k = 2
+  when "休講"
+  k = 3
   end
  attend[username][i][j][k] += 1
  end
 end
+
 #見る
-def attend_checker(contents,username,status,id)
+
+def attend_checker(contents,username,id)
  attend = PStore.new('attend.ps')
  attend.transaction do
   if attend[username] == nil
-   attend[username]=[[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],],]
+   attend[username]=[[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],],]
 
   end
 contents = contents.gsub(/@kasumikobot 出席管理|\s|　/,"")
@@ -68,7 +72,8 @@ contents = contents.gsub(/@kasumikobot 出席管理|\s|　/,"")
   when "金曜"
   i = 4
   end
-cont = "#{"@"+username} あなたの#{contents[0,2]}の出席情報です。\n1限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}休講\n2限 #{attend[username][i][1][0]}出席 #{attend[username][i][1][1]}欠席 #{attend[username][i][1][2]}休講\n3限 #{attend[username][i][2][0]}出席 #{attend[username][i][2][1]}欠席 #{attend[username][i][2][2]}休講\n4限 #{attend[username][i][3][0]}出席 #{attend[username][i][3][1]}欠席 #{attend[username][i][3][2]}休講\n5限 #{attend[username][i][4][0]}出席 #{attend[username][i][4][1]}欠席 #{attend[username][i][4][2]}休講\n6限 #{attend[username][i][5][0]}出席 #{attend[username][i][5][1]}欠席 #{attend[username][i][5][2]}休講\n"
+cont = "#{"@"+username} あなたの#{contents[0,2]}の出席情報です。\n1限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n2限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n3限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n4限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n5限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n6限 #{attend[username][i][0][0]}出席 #{attend[username][i][0][1]}欠席 #{attend[username][i][0][2]}遅刻 #{attend[username][i][0][3]}休講\n"
 reply(cont,id)
  end
 end
+
